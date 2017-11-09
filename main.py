b49bd89980aa66af376f60bbf754f630a949b8b4
Michael Stajer <michael@stajer.net>
2017-11-08 22:31:08 -0800

    html setup
    setup base html template and initial subscribe buttons using stripe.js

--- main.py
+++ main.py
@@ -15,11 +15,22 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    autoescape=True,
+    extensions=['jinja2.ext.autoescape'])
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        variables = {}
+        template = "templates/main.html"
+        template = JINJA_ENVIRONMENT.get_template(template)
+        self.response.write(template.render(variables))
 
 
 app = webapp2.WSGIApplication([
